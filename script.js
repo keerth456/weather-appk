8c2425e022c5a4d8939a89c712a92d4d54712226
keerthana K <keerthana4111986@gmail.com>
2025-10-07 22:27:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 async function getweather()     
   const city =
-document.ElementById("city").value;
-   const resultdiv =
+document.getElementById("city").value;
+   const resultiDv =
 document.getElementById("result");
 
 if (city === " ") {
-  resultDiv.innerHTML = "Please enter a city name 🌧️";
-  return;
+  resultDiv.innerHTML = "⚠️Please enter a city name 🌆";
+  return; 
 }
- const apikey ="https://wttr.in/" + city + "?format=%t;
+ const apiUrl ="https://wttr.in/" + city + "?format=%t";
 
 try {
-  const response = await fetch(apikey);
+  const response = await fetch(apiUrl);
   const data = await response.text();
-  resultDiv.innerHTML = 'Weather in ${city}: ${data}:
+  resultDiv.innerHTML = ' ⛅Weather in ${city}: ${data}:
 } catch (error) {
-  result Div.innerHTML = "could not get weather data ❌";
+  resultDiv.innerHTML = "❌ Could not get weather data. Try again!";
 }
 }
